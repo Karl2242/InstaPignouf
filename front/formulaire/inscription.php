7d8f914bc16b6ef84a8a6442c3b0120aa4b83d9a
Simoncastagne42 <simoncastagne42@gmail.com>
2024-12-18 14:12:00 +0100

    base architecture

--- front/formulaire/inscription.php
+++ front/formulaire/inscription.php
@@ -12,7 +12,7 @@
 <div class="w-full flex flex-col justify-center items-center mt-24 gap-6 text-center">
 <header class="flex flex-col gap-10">
 <img class="w-52" src="../../image/logo.webp" alt="logo">
-<h3 class="text-3xl font-jaro">Connexion</h3>
+<h3 class="text-3xl font-jaro">Inscription</h3>
 </header>
 <main class="flex flex-col">
 
@@ -32,11 +32,14 @@
 ?>
 <form class="flex flex-col justify-center gap-6 w-full items-center" action="./process/process_inscription.php" method="post">
     <input class="text-white font-extralight w-[350px] pl-4 bg-couleur-input py-3 border-white border-[1px] border-opacity-[20%] rounded-lg" type="email" id="email" name="email" placeholder="Email" required>
+    <input class="text-white font-extralight w-[350px] pl-4 bg-couleur-input py-3 border-white border-[1px] border-opacity-[20%] rounded-lg" type="text" id="username" name="username" placeholder="Nom d'utilisateur" required>
+    <input class="text-white font-extralight w-[350px] pl-4 bg-couleur-input py-3 border-white border-[1px] border-opacity-[20%] rounded-lg" type="text" id="prenom" name="prenom" placeholder="Prenom" required>
+    <input class="text-white font-extralight w-[350px] pl-4 bg-couleur-input py-3 border-white border-[1px] border-opacity-[20%] rounded-lg" type="text" id="name" name="name" placeholder="Nom de famille" required>
     <input class="text-white font-extralight w-[350px] pl-4 bg-couleur-input py-3 border-white border-[1px] border-opacity-[20%] rounded-lg" type="password" id="password" name="password" placeholder="Mot de passe" required>
-    <input class="bg-[#df6072] p-2 py-3 w-[70%] cursor-pointer mt-4 rounded-2xl shadow-xl" type="submit" value="Se connecter">
+    <input class="bg-[#df6072] p-2 py-3 w-[70%] cursor-pointer mt-4 rounded-2xl shadow-xl" type="submit">
 </form>
 
-<p class="pt-5 font-extralight">Pas encore de compte ?<a class="text-[#df6072]" href="../formulaire/inscription.php"> Inscrivez vous</a></p>
+<p class="pt-5 font-extralight">Dejà un compte? <a class="text-[#df6072]" href="./login.php">Connectez vous</a></p>
 
 </main>
 
