319bc1575ddb7c8e9bf51feed82d348d11c05592
Trapo6x7 <matoufdanslezoo@gmail.com>
2024-12-19 12:06:52 +0100

    css mathias

--- utils/front/haut.php
+++ utils/front/haut.php
@@ -6,5 +6,6 @@
     <title>Document</title>
     <link rel="stylesheet" href="../../css/output.css">
     <link rel="icon" href="../../image/logo.webp">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body class="bg-noir-bleu text-white">
